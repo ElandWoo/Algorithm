ff0bc17b2b722d896800423b51fddc41d3ca2514
Eland <86370026+ElandWoo@users.noreply.github.com>
2022-04-28 08:20:03 +0800

    Update 1.cpp

--- leetcode/cpp/1.cpp
+++ leetcode/cpp/1.cpp
@@ -65,22 +65,19 @@ public:
   
   
   /*
+  the same as last solution.
   */
-      vector<int> twoSum(vector<int> &numbers, int target) {
+    vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> m;
-        vector<int> result;
-        for(int i=0; i<numbers.size(); i++){
-            // not found the second one
-            if (m.find(numbers[i])==m.end() ) { 
-                // store the first one position into the second one's key
-                m[target - numbers[i]] = i; 
-            }else { 
-                // found the second one
-                result.push_back(m[numbers[i]]+1);
-                result.push_back(i+1);
-                break;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            // if target - nums[i] in m, just return the ans;
+            // if not store one position into the second one's key.
+            if (m.find(nums[i]) != m.end()) {
+                return {m[nums[i]], i};
             }
+            m[target - nums[i]] = i;
         }
-        return result;
+        return {};
     }
 };
